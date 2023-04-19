64e843ba332e7838cca5f834de9abceb4e438a77
hieu22012003 <101488865+hieu22012003@users.noreply.github.com>
2023-04-19 09:22:11 +0700

    Update gioithieu.blade.php

--- resources/views/client/gioithieu.blade.php
+++ resources/views/client/gioithieu.blade.php
@@ -93,7 +93,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
                         <div class="team-item bg-white mb-4">
                             <div class="team-img position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/329739756_1391637348273153_1241118877991390923_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=-lbKFQuVyIwAX_eOffK&_nc_ht=scontent.fhan17-1.fna&oh=00_AfD-8uau35QlI02oYsN33oWQmaBr7b3438_jmlLERSLG4w&oe=643D28DA" alt="">
+                                <img class="img-fluid w-100" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/319664624_5871652436188464_7356375749769381303_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=FFmhyAxI98QAX8fv7tE&_nc_ht=scontent.fhan17-1.fna&oh=00_AfBdZoD4syT5Pvci8tmf4s_2GFn25c-eTeLRp_PteCpKRw&oe=64438671" alt="">
                                 <div class="team-social">
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-twitter"></i></a>
                                     <a class="btn btn-outline-primary btn-square" href="https://www.facebook.com/youngspidey2301"><i class="fab fa-facebook-f"></i></a>
@@ -127,16 +127,16 @@
                     <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
                         <div class="team-item bg-white mb-4">
                             <div class="team-img position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/325470590_3077613635872273_9207768098266673125_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_ohc=pGj9Pf70VTAAX9cIPYC&_nc_ht=scontent.fhan17-1.fna&oh=00_AfDIhtn5Ce1IrKIgkgmmlaYznEerUDRaAwR0ouAuR13CCA&oe=643CF20E" alt="">
+                                <img class="img-fluid w-100" src="https://images2.thanhnien.vn/zoom/686_429/Uploaded/dotuan/2022_08_04/1-9730.jpg" alt="">
                                 <div class="team-social">
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-outline-primary btn-square" href="https://www.facebook.com/youngspidey2301/"><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href="https://www.facebook.com/vu.theanh.376043"><i class="fab fa-facebook-f"></i></a>
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-instagram"></i></a>
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
                             <div class="text-center py-4">
-                                <h5 class="text-truncate">LIONEL MESSI</h5>
+                                <h5 class="text-truncate">VŨ THẾ ANH</h5>
                                 <p class="m-0">Developer</p>
                             </div>
                         </div>
@@ -144,16 +144,16 @@
                     <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
                         <div class="team-item bg-white mb-4">
                             <div class="team-img position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="https://vcdn-thethao.vnecdn.net/2022/06/27/dejong-7549-1656338602.jpg" alt="">
+                                <img class="img-fluid w-100" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/319539708_1108536289805280_6144920764314498105_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=z1F1uOYaV_QAX842dkG&_nc_ht=scontent.fhan17-1.fna&oh=00_AfDF8EnyNFlnNf9NcBgM9YM94xU7VN9TsQpeuZZTLUh4Tw&oe=64438D8F" alt="">
                                 <div class="team-social">
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-outline-primary btn-square" href="hhttps://www.facebook.com/youngspidey2301/"><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href="hhttps://www.facebook.com/dangpham.danh.9"><i class="fab fa-facebook-f"></i></a>
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-instagram"></i></a>
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
                             <div class="text-center py-4">
-                                <h5 class="text-truncate">FRENKIE DE JONG</h5>
+                                <h5 class="text-truncate">PHẠM ĐĂNG DANH</h5>
                                 <p class="m-0">Developer</p>
                             </div>
                         </div>
@@ -161,17 +161,17 @@
                     <div class="col-lg-4 col-md-4 col-sm-6 pb-1">
                         <div class="team-item bg-white mb-4">
                             <div class="team-img position-relative overflow-hidden">
-                                <img class="img-fluid w-100" src="https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/213_cruyff.jpg" alt="">
+                                <img class="img-fluid w-100" src="https://scontent.fhan17-1.fna.fbcdn.net/v/t1.15752-9/335026861_652019923350145_6223389702177717614_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=R3BEZuDML8IAX_CLcpv&_nc_ht=scontent.fhan17-1.fna&oh=03_AdQzoEFsJaKYwh6T9klE1cYxKJBDMz-q2UJ0MsMCMUkwHg&oe=6466A1CC" alt="">
                                 <div class="team-social">
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-outline-primary btn-square" href="https://www.facebook.com/youngspidey2301/"><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href="https://www.facebook.com/quyen.nhu.56027281"><i class="fab fa-facebook-f"></i></a>
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-instagram"></i></a>
                                     <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-linkedin-in"></i></a>
                                 </div>
                             </div>
                             <div class="text-center py-4">
-                                <h5 class="text-truncate">JOHAN CRUYFF</h5>
-                                <p class="m-0">BE Developer</p>
+                                <h5 class="text-truncate">NHỮ TIẾN QUYỀN</h5>
+                                <p class="m-0">Developer</p>
                             </div>
                         </div>
                     </div>
@@ -182,119 +182,6 @@
 
 
 
-        <!-- Footer start-->
-
-
-        <section id="end-page">
-            <div class="footer py-5 bg-dark">
-                <div class="container">
-
-                    <div class="row">
-                        <div class="col-md-4">
-
-                            <a href="#" class="navbar-brand">
-                                <h2 class="m-0 text-primary"><span class="text-white">BANANA</span> AIRLINES  </h2>
-                            </a>
-                            <br> <br>
-                            <p class="text-white">
-                                Chúng tôi cam kết mang lại những trải nghiệm tuyệt vời đến các bạn.
-
-                            </p>
-
-                            <p class="text-white">
-                                Xin trân trọng cám ơn sự đồng hành của Qúy khách!
-                            </p>
-                            <div class="d-flex justify-content-start mt-4">
-                                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-                                   style="width: 38px; height: 38px;" href="#"><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-                                   style="width: 38px; height: 38px;" href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-                                   style="width: 38px; height: 38px;" href="#"><i class="fab fa-linkedin-in"></i></a>
-                                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-                                   style="width: 38px; height: 38px;" href="#"><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-
-                        <div class="ml-auto d-md-block d-none">
-
-
-                            <span class="ti-search search-icon text-light"></span>
-                        </div>
-
-
-
-
-                        <div class="col-md-3">
-                            <h4 class="text-white">Thông tin thêm</h4>
-                            <nav class="nav flex-column my-3 footer-web-info">
-                                <a style="color: #ffffff;"
-                                   href="#"
-                                   title="Điều khoản sử dụng')}}">Điều khoản sử dụng</a>
-                                <a style="color: #ffffff;"
-                                   href="#"
-                                   title="Chính sách bảo mật">Chính sách bảo mật</a>
-                                <a style="color: #ffffff;"
-                                   href="#"
-                                   title="Hướng dẫn đặt vé">Hướng dẫn đặt vé</a>
-                                <a style="color: #ffffff;"
-                                   href="#"
-                                   title="Hướng dẫn đặt vé">Hướng dẫn thanh toán</a>
-                            </nav>
-                        </div>
-
-                        <div class="col-md-2">
-                            <h4 class="text-white">Hỗ trợ</h4>
-                            <nav class="nav flex-column my-3 footer-web-info">
-                                <a style="color: #ffffff;"
-                                   href="javascript:void 0"
-                                   title="Phản hồi">Phản hồi</a>
-                                <a style="color: #ffffff;"
-                                   href="#"
-                                   title="Câu hỏi thường gặp">Câu hỏi thường gặp</a>
-                                <a style="color: #ffffff;"
-                                   href="#"
-                                   title="Tư vấn hỗ trợ đặt vé">Tư vấn hỗ trợ đặt vé</a>
-                            </nav>
-                        </div>
-                        <div class="col-md-3">
-                            <h4 class="text-white">Liên hệ với chúng tôi</h4>
-                            <br>
-                            <div class="d-flex">
-                                <h4 class="fa fa-map-marker-alt text-primary"></h4>
-                                <div class="pl-3">
-                                    <h5 style="color: #D9a404;">Địa chỉ</h5>
-                                    <p style="color: #ffffff;">236, Hoàng Quốc Việt, Bắc Từ Liêm, Hà Nội</p>
-                                </div>
-                            </div>
-                            <div class="d-flex">
-                                <h4 class="fa fa-envelope text-primary"></h4>
-                                <div class="pl-3">
-                                    <h5 style="color: #D9a404;">Email</h5>
-                                    <p style="color: #ffffff;">customer@banana_airline.com</p>
-                                </div>
-                            </div>
-                            <div class="d-flex">
-                                <h4 class="fa fa-phone-alt text-primary"></h4>
-                                <div class="pl-3">
-                                    <h5 style="color: #D9a404;">Hotline</h5>
-                                    <p style="color: #ffffff;">+012 345 67890</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr color="white" align="right" size="5px">
-                <div class="container-fluid p-0 " style="color: #20211F;">
-                    <p class="text-center" style="color: #ffffff">
-                        <a>Công ty TNHH BANANA AIRLINE</a> <br>
-                        <a>Coppyright</a>&copy;<a class="text-primary font-weight-bold" href="#"> 2022 BANANA </a> <br>
-                    </p>
-                </div>
-            </div>
-        </section>
-        <!-- Footer End -->
-        <!-- Back to Top -->
         <a href="about.html" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
 
 
